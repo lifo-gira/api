ac282dfa18fca95b234e0ca31311ef907ed687e8
lifo-gira <130842622+lifo-gira@users.noreply.github.com>
2023-08-19 23:23:11 +0530

    Update app.py
    change the post metrics method to get metrics method with id addition

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ async def addData( data: Data):
     res = await db.putData(data=data)
     return{"dataCreated": res}
 
-@app.post("/metrics")
+@app.get("/metrics/{id}")
 async def getData(data_id: list):
     res = await db.getData(data_id)
     return res
