8db5a55dd5990f78d5a03bb7bb39b153deff611d
AnirudhPMenon <anirudh20017@gmail.com>
2023-08-28 22:09:53 +0530

    websockets

--- app.py
+++ app.py
@@ -26,65 +26,65 @@ def root():
     return {"Message": "use '/docs' endpoint to find all the api related docs "}
 
 
-# @app.post("/post-data/{data}")
-# def postData(data):
-#     try:
-#         storedData.append(data)
-#         return {"inserted": "true"}
+@app.post("/post-data/{data}")
+def postData(data):
+    try:
+        storedData.append(data)
+        return {"inserted": "true"}
 
-#     except:
-#         return {"inserted": "false"}
+    except:
+        return {"inserted": "false"}
 
-# @app.get("/get-all-data")
-# def getData():
-#     return storedData
+@app.get("/get-all-data")
+def getData():
+    return storedData
 
     
-# @app.post("/create-adminuser")
-# async def createAdminUser(data: Admin):
-#     res = await db.createAdminUser(data=data)
-#     return{"userCreated": res} 
+@app.post("/create-adminuser")
+async def createAdminUser(data: Admin):
+    res = await db.createAdminUser(data=data)
+    return{"userCreated": res} 
 
-# @app.post("/create-doctor")
-# async def createDoctor(data: Doctor):
-#     res = await db.createDoctor(data=data)
-#     return{"userCreated": res}
+@app.post("/create-doctor")
+async def createDoctor(data: Doctor):
+    res = await db.createDoctor(data=data)
+    return{"userCreated": res}
 
-# @app.post("/create-patient")
-# async def createPatient(data: Patient):
-#     res = await db.createPatient(data=data)
-#     return{"userCreated": res}
+@app.post("/create-patient")
+async def createPatient(data: Patient):
+    res = await db.createPatient(data=data)
+    return{"userCreated": res}
 
-# @app.post("/login")
-# async def initiateLogin(user_id: str, password: str):
-#     res = await db.loginUser(user_id, password) 
-#     if res["loginStatus"] == True:
-#         return res["data"]
+@app.post("/login")
+async def initiateLogin(user_id: str, password: str):
+    res = await db.loginUser(user_id, password) 
+    if res["loginStatus"] == True:
+        return res["data"]
 
-# @app.get("/get-all-user/{type}")
-# async def getUsers(type: Literal["admin", "doctor", "patient", "all"]):
-#     res = await db.getAllUser(type)
-#     return res
+@app.get("/get-all-user/{type}")
+async def getUsers(type: Literal["admin", "doctor", "patient", "all"]):
+    res = await db.getAllUser(type)
+    return res
 
-# @app.get("/get-user/{type}/{id}")
-# async def getUsers(type: Literal["admin", "doctor", "patient"], id: str):
-#     res = await db.getUser(type, id)
-#     return res
+@app.get("/get-user/{type}/{id}")
+async def getUsers(type: Literal["admin", "doctor", "patient"], id: str):
+    res = await db.getUser(type, id)
+    return res
 
-# @app.post("/post-data")
-# async def addData(user_id: str,data: Data):
-#     res = await db.postData(user_id=user_id, data=data)
-#     return{"dataCreated": res}
+@app.post("/post-data")
+async def addData(user_id: str,data: Data):
+    res = await db.postData(user_id=user_id, data=data)
+    return{"dataCreated": res}
 
-# @app.put("/put-data")
-# async def addData( data: Data):
-#     res = await db.putData(data=data)
-#     return{"dataCreated": res}
+@app.put("/put-data")
+async def addData( data: Data):
+    res = await db.putData(data=data)
+    return{"dataCreated": res}
 
-# @app.post("/metrics")
-# async def getData(data_id: list):
-#     res = await db.getData(data_id)
-#     return res
+@app.post("/metrics")
+async def getData(data_id: list):
+    res = await db.getData(data_id)
+    return res
 
 @app.websocket("/ws-get-user/{type}/{id}")
 async def websocket_endpoint(type: Literal["admin", "doctor", "patient"], id: str, websocket: WebSocket):
